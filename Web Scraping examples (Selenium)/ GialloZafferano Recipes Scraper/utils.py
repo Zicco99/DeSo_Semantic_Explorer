814889f77f116ecc06d1e71d175f02967de249a9
Francesco Giuseppe Ziccolella <52136996+Zicco99@users.noreply.github.com>
2022-08-30 20:05:58 +0200

    Add files via upload

---
+++ Web Scraping examples (Selenium)/ GialloZafferano Recipes Scraper/utils.py
@@ -0,0 +1,13 @@
+import json
+
+
+keys = {
+    # PERMANENT INFO
+    'email': '---',
+    'password': '---',
+    'scrap_page' : 'https://www.giallozafferano.it/ricette-cat/',
+    'recipe_categ': {
+        'antipasti': '/Antipasti/',
+    }
+
+}
